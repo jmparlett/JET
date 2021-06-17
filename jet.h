378291524dbd98b57e8a2bb30236bb49e34838a8
Jonathan Parlett <parlettjon@gmail.com>
2021-06-16 22:01:04 -0400

    added inputing all acii chars, and backspaceing, currently implementing enter functionality for adding new lines

--- jet.h
+++ jet.h
@@ -7,6 +7,52 @@
 
 /*******KEY CODES*******/
 enum editorkeys{
-	QUIT = 17 /* ctr-q */
+	QUIT = 17, /* ctr-q */
+	BKSPC = 127 /* backspace */
 
 };
+
+
+/********************************** Editor Structure *****************************/
+typedef struct frow{ /* file row struct */
+
+	int idx; /* line num in file */
+	
+	int len; /* length of line */
+
+	int mlen; /* max length */
+	
+	struct frow *nx; /* next node */
+
+	struct frow *pr; /* prev node */
+
+	char *s; /* string of line */
+
+} frow;
+
+typedef struct editor{
+
+	int cx, cy; /* position */
+	int rowc;   /*num rows */
+	int colc;   /*num cols */
+	int drawstart; /* position to begin drawing each line. Used for truncation */
+	frow *fline; /*first line/ head of linked list */
+	frow *cline; /* current line */
+	frow *tsl; /* top screen line */
+	
+} editor;
+
+
+
+/*************** UTIL FUNCS/MACROS *******************/
+#define typeable(A) ((A) >= 32 && (A) < 127)
+
+int extendline(frow *line, int len){/* extend current line by a given len (num of chars) */
+	line->mlen += len;
+
+	if((line->s = (char *)realloc(line->s, line->mlen)) == NULL) return -1; 
+
+	return 0;
+}
+
+
